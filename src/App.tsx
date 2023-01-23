30752d2e89df1221abcb382956ae71ba8f7e9491
Braxton Francom <braxtonfrancom@Braxtons-MacBook-Pro.local>
2023-01-23 10:11:21 -0700

    UI styled, search functionality working

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,115 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src="/vite.svg" className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
-
-// export default App 
+import React from 'react'
 
 
 
-const result = await fetch("https://api.quotable.io/random");
-      console.log(await result.json());
+interface Quote {
+  quote: string;
+  author: string;
 
-
-function GrabQuotes() {  //add async at start
-  
-  // const result = await fetch("https://api.quotable.io/random");
-  // console.log(await result.json());
-  return <div>I am a component</div>
 }
 
-
-
 export function App() {
+
+  const [quoteContent, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [listQuotesBool, setListQuotesBool] = useState(false)
+  const [listQuotes, setListQuotes] = useState([''])
+  const [listQuotes2, setListQuotes2] = useState([''])
+  const [listQuotes3, setListQuotes3] = useState([''])
+
+  const [searchWord, setSearchWord] = useState('')
+  
+
+
+  async function callApi() {  //add async at start
+    setLoading(true);
+    const result = await fetch("https://api.quotable.io/random");
+    const quoteObject = await result.json();
+    await timeout(500);
+    setLoading(false);
+    setQuote(quoteObject.content);
+    setAuthor(quoteObject.author);
+  }
+
+  async function callSearchApi() {
+    setListQuotesBool(true);
+    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/search?query=" + searchWord);    //https://the-trivia-api.com/api/questions 
+    const quotesObject = await result.json();
+    // console.log(await result.json());
+    console.log(quotesObject.results)
+    //console.log(searchWord)
+    setListQuotes(quotesObject.results[0].content + quotesObject.results[0].author)
+    setListQuotes2(quotesObject.results[1].content + quotesObject.results[1].author)
+    setListQuotes3(quotesObject.results[2].content + quotesObject.results[2].author)
+
+
+    //console.log(quotesObject[0].question)
+    // setListQuotes(quotesObject[0].question + quotesObject[0].correctAnswer); //[0].question    //["bru", "bru"]
+    // setListQuotes2(quotesObject[1].question + quotesObject[1].correctAnswer)
+    // setListQuotes3(quotesObject[2].question + quotesObject[2].correctAnswer)
+  }
+
+
+
+  function timeout(delay: number) {
+    return new Promise( res => setTimeout(res, delay) );
+}
+
+  useEffect(() => { //On the first time you render, do this, and then never do it again
+    callApi()
+    
+  },[])  //If you put something in the array, ex: [author] --> THIS means that anytime the author is changed, callApi() is called 
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchWord(searchWord);
+    callSearchApi()
+  };
+
+
   return (
     <div>
       <h1>Quote Search</h1>
-      <form id="form">
-        <input type="search" placeholder="Search..."/>
+      <form id="form" onSubmit={handleSubmit}> {/*onSubmit={handleSubmit}*/}
+        <input 
+        type="search" 
+        placeholder="Search..." 
+        id="search" 
+        onChange={(event) =>
+          setSearchWord(event.target.value)
+        }
+        // onSubmit={  
+        //   (e) => handleSubmit(e)
+          
+        // }
+        // onChange={(e) => setListQuotes(e.target.valueAsNumber)}
+
+         />                      {/*(e) => e.preventDefault()*/}
       </form>
-      <h2>Random quote here</h2>
-      <GrabQuotes/>
+      <div>  
+        {
+          <div>
+            {listQuotesBool ? <>
+            <h3 className='listItem'>{listQuotes}</h3>
+            <h3 className='listItem'>{listQuotes2}</h3>
+            <h3 className='listItem'>{listQuotes3}</h3></> : <><h3>{loading ? <>Loading..</> : <>{quoteContent}</>}</h3>
+            <h4>{author}</h4></>}
+
+
+            <h4>{searchWord}</h4>
+          </div>
+        }
+      </div>
     </div>
   )
 }
+
+
+{/*gitignore the node_modules!
+
+SIde Effect*/}
